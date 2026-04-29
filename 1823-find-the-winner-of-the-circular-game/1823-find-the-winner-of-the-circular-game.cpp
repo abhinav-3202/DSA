a8e91166cb29b0e4a31c51341aa8bf6dcb9b9e6b
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-04-29 21:39:09 +0530

    Time: 68 ms (5.32%), Space: 7.9 MB (76.86%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,11 +1,34 @@
 class Solution {
 public:
-    int check(int n ,int k){
-        if(n==1) return 0;//agar 1 hi person h then wo hi winner hoga 
-        // we got the winner now adding people would shift the indexing so , now fix it 
-        return (check(n-1,k)+k)%n;
+    int falseCount=0;
+    void check(vector<bool>&vis,int x,int k,int i,int n){
+        if(falseCount == (n-1)) return;
+        if(vis[i%n]==false) check(vis,x,k,i+1,n);
+        else{
+            x = x+1;
+            if(x==k){
+                vis[i%n] = false;
+                x = 0;
+                falseCount++;
+                check(vis,x,k,i+1,n);
+            }
+            else{
+                check(vis,x,k,i+1,n);
+            }
+        }
+        return ;
     }
     int findTheWinner(int n, int k) {
-        return check(n,k)+1;
+        vector<bool>vis(n,true);
+        int x = 0;
+        check(vis,x,k,0,n);
+        int ans =-1;
+        for(int i = 0 ; i<vis.size();i++){
+            if(vis[i]==true){
+                ans = i;
+                break;
+            }
+        }
+        return ans+1;
     }
 };
