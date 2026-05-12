70221a0e63e1111f2a2de47820ecbd6229cce9c8
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-05-12 20:23:41 +0530

    Time: 0 ms (100%), Space: 28.1 MB (86.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,6 +3,8 @@ public:
     int majorityElement(vector<int>& nums) {
         int ans = nums[0];
         int count = 0;
+        // if the choosen elemnt will be majority element then it would not have been cancelled or the count come to 0....
+        // hence at the last u will always get the majority element because its count will never be 0
         for(int i =0;i<nums.size();i++){
             if(count==0) ans = nums[i];
             if(nums[i]==ans){
