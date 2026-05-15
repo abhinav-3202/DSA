527df780dcc6510e82f415dbc73d9cbc00ecf2ce
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-05-15 09:56:09 +0530

    Time: 0 ms (100%), Space: 9.4 MB (50.51%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,6 +13,19 @@ public:
         int xx = 0;
         int xy = 0;
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return check(dp,m,n,xx,xy);
+        for(int i = 0;i<=m-1;i++){
+            for(int j = 0;j<=n-1;j++){
+                if(i==0&&j==0) dp[i][j]=1;
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+        // return check(dp,m,n,xx,xy);
     }
 };
