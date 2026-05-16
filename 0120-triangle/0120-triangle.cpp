591cbb30866cf6465b9b189dd566f7d6de87fb26
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-05-16 10:13:32 +0530

    Time: 0 ms (100%), Space: 13.5 MB (7.82%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -9,6 +9,8 @@ public:
                 else{
                     int up = INT_MAX;
                     int left = INT_MAX;
+// the reason below we have done j<=i-1 because if we go up in the i-1 row j will not be there 
+// i=1 j=1 , in i-1 j=1 not available
                     if(i>0 && j<=i-1) up= dp[i-1][j];
                     if(j>0 ) left = dp[i-1][j-1];
                     dp[i][j]=triangle[i][j]+min(up,left);
@@ -16,9 +18,13 @@ public:
             }
         }
         int maxi =INT_MAX;
+        // maximum value bottom ke kisi pe bhi ho skti h 
         for(int i =0;i<n;i++){
             maxi=min(maxi,dp[n-1][i]);
         }
         return maxi;
     }
 };
+
+//dp[i][j] represents the minimum path sum required to reach the cell (i, j) starting from (0, 0)
+//The History: It accumulates the values of the best possible choices made at every row above it.
