a6555a0844cb541a8873e9fb92102f85ab5bcd81
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-05-28 19:44:53 +0530

    Time: 23 ms (70.9%), Space: 36.7 MB (47.25%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
+    int check(vector<int>&coins,vector<vector<int>>&dp,int amount,int i){
+        if(amount==0) return 1;
+        if(i==0){
+            if(amount%coins[0]==0) return 1;
+            else return 0;
+        }
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int notTake = check(coins,dp,amount,i-1);
+        int take = 0;
+        if(coins[i]<=amount) {
+            take=check(coins,dp,amount-coins[i],i);
+        }
+        return dp[i][amount] = take+notTake;
+    }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<unsigned int>>dp(n+1,vector<unsigned int>(amount+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=amount;j++){
-                unsigned int notTake=dp[i-1][j];
-                unsigned int take=0;
-                if(coins[i-1]<=j){
-                    take=dp[i][j-coins[i-1]];
-                }
-                dp[i][j]=take+notTake;
-            }
-        }
-        return dp[n][amount];
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int res= check(coins,dp,amount,n-1);
+        return res;
     }
 };
