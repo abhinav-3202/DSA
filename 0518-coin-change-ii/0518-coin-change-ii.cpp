a22d1ab8dbc7b33775356b3f0dec211bd00a2a96
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-05-28 07:24:21 +0530

    Time: 27 ms (64%), Space: 36.5 MB (60%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int check(vector<int>&coins,vector<vector<int>>&dp,int amount,int i){
+        if(amount==0) return 1;
+        if(i==0){
+            if(amount%coins[0]==0) return 1;
+            else return INT_MAX;
+        }
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int notTake = check(coins,dp,amount,i-1);
+        int take = INT_MAX;
+        if(coins[i]<=amount) {
+            take=check(coins,dp,amount-coins[i],i);
+        }
+        if(notTake==INT_MAX && take==INT_MAX){
+            return dp[i][amount] = INT_MAX;
+        }
+        else if(take==INT_MAX) return dp[i][amount] = notTake;
+        else if(notTake==INT_MAX) return dp[i][amount] =take;
+        return dp[i][amount] = take+notTake;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int res= check(coins,dp,amount,n-1);
+        if(res==INT_MAX) return 0;
+        return res;
+    }
+};
