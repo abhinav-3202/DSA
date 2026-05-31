0977e84f28f261533293804241b0d9421f8f0c43
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-05-31 18:31:52 +0530

    Time: 52 ms (5.84%), Space: 106.8 MB (16.2%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        int n=asteroids.size();
+        long long mas=mass;
+        sort(asteroids.begin(),asteroids.end());
+        for(int i=0;i<n;i++){
+            if(mas<asteroids[i]) return false;
+            else{
+                mas+= asteroids[i];
+            }
+        }
+        return true;
+    }
+};
