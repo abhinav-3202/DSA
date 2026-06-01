c4df4f39974900e539ff5ab8bad053dfd39668b2
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-01 19:44:02 +0530

    Time: 15 ms (31.15%), Space: 13.9 MB (95.41%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(),nums.end());
         int close=nums[0]+nums[1]+nums[2];
         for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+            // if(i>0 && nums[i]==nums[i-1]) continue;
             int j=i+1;
             int k=n-1;
             while(j<k){
