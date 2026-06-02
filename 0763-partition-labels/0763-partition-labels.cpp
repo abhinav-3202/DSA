07d13b378b81b8909f9cb99b57d0970f31061766
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-02 10:19:35 +0530

    Time: 0 ms (100%), Space: 8.9 MB (65.44%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<int,int>mpp;
-        for(int i=0;i<s.size();i++){
-            mpp[s[i]]++;
-        }
-        int end=0;
-        unordered_map<int,int>mppp;
+        vector<int>t(26,0);
         vector<int>ans;
-        for(int i =0;i<s.size();i++){
-            mppp[s[i]]++;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            t[s[i]-'a']=i;
+            // storing the last index of every char
+        }
+        int start=0;
+        int end=0;
+        unordered_map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            mpp[s[i]-'a']=i;
             bool flag = true;
-            for(auto it:mppp){
-                if(mpp[it.first]!=it.second){
+            for(auto it:mpp){
+                if(it.second!=t[it.first]){
                     flag=false;
                     break;
                 }
             }
             if(flag==true){
-                mppp.clear();
+                mpp.clear();
                 ans.push_back(i-end+1);
                 end=i+1;
             }
