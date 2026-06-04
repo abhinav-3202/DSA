0b84a4ee4e077a34a69023fdf310fc864dff7b0e
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-04 19:25:00 +0530

    Time: 86 ms (42.77%), Space: 76.1 MB (7.61%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -15,16 +15,20 @@ public:
         string t=s;
         rev(t);
         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        int ans = INT_MIN;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(s[i-1]==t[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
+                    // ans=max(ans,dp[i][j]);
                 }
+                // in case of substring or subarray it will be dp[i][j]=0, because ek bhi same nhi rha then subarray will not form , but for subsequence this can be carried on 
                 else{
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
         return dp[n][n];
+        // return ans;
     }
 };
