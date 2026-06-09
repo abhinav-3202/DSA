2422d9d162181e151847d1b9d5a8d1d486817808
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-09 07:30:54 +0530

    Time: 38 ms (22.81%), Space: 26.9 MB (70.23%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int check(string& s,string &t , int i,int j,int m,int n ,vector<vector<int>>&dp){
-        if(j==n) return 1;
+        if(j==n) return 1;//second string t end ho gyi , means subsequence matched 
         if(i>=m) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]){
+        if(s[i]==t[j]){//match mil gya so dono ko ek ek badha do means pick it 
             dp[i][j]= check(s,t,i+1,j+1,m,n,dp)+
             check(s,t,i+1,j,m,n,dp);
+            // match mila then also not pick
         }
         else{
             dp[i][j]=check(s,t,i+1,j,m,n,dp);
+            //match nhi mila then not pick obviously
         }
         return dp[i][j];
     }
