47b1ac8d06f95cdc934c659486a058df716dd0a1
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-09 10:20:52 +0530

    Time: 0 ms (100%), Space: 10 MB (58.59%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    void check(vector<int>&nums,int j,int n,vector<int>&temp,set<vector<int>>&st){
+    void check(vector<int>&nums,int j,int n,vector<int>&temp,vector<vector<int>>&ans){
         if(j>=n){
             // st.insert(temp);
             return;
         }
         for(int i=j;i<n;i++){
             temp.push_back(nums[i]);
-            st.insert(temp);
-            check(nums,i+1,n,temp,st);
+            ans.push_back(temp);
+            check(nums,i+1,n,temp,ans);
             temp.pop_back();
         }
         return;
@@ -16,13 +16,9 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n=nums.size();
         vector<int>temp;
-        set<vector<int>>st;
-        st.insert(temp);
-        check(nums,0,n,temp,st);
         vector<vector<int>>ans;
-        for(auto it:st){
-            ans.push_back(it);
-        }
+        ans.push_back(temp);
+        check(nums,0,n,temp,ans);
         return ans;
     }
 };
