0c2cefda0e0fb40f0a813c9c54624a07600980fd
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-10 19:44:50 +0530

    Time: 22 ms (75.98%), Space: 26.9 MB (70.41%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -21,19 +21,19 @@ public:
         vector<vector<unsigned int>>dp(m+1,vector< unsigned int>(n+1,0));
         // int count = check(s,t,0,0,m,n,dp);
         for(int i=0;i<=m;i++){
-            dp[i][0]=1;
+            dp[i][n]=1;
         }// if the length of t string is 0 then any length of s string can form subsequence 
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(s[i-1]==t[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(s[i]==t[j]){
+                    dp[i][j]=dp[i+1][j+1]+dp[i+1][j];
                 }
                 else {
-                    dp[i][j]=dp[i-1][j];
+                    dp[i][j]=dp[i+1][j];
                 }
             }
         }
-        return dp[m][n];
+        return dp[0][0];
         // return count;
     }
 };
