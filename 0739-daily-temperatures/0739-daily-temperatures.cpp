7b9d64f02ca6b6ac70bbb3d3547731f362ff69a4
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-10 10:53:04 +0530

    Time: 11 ms (94.3%), Space: 102.9 MB (80.88%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -4,25 +4,20 @@ public:
         int n=temperatures.size();
         vector<int>ans(n,0);
         stack<int>st1;
-        stack<int>st2;
-        st1.push(temperatures[n-1]);
-        st2.push(n-1);
+        st1.push(n-1);
         ans[n-1]=0;
         int i=n-2;
         while(i>=0){
-            while(!st1.empty()&&!st2.empty()&&st1.top()<=temperatures[i]){
+            while(!st1.empty()&&temperatures[st1.top()]<=temperatures[i]){
                 st1.pop();
-                st2.pop();
             }
             if(st1.empty()) {
                 ans[i]=0;
-                st1.push(temperatures[i]);
-                st2.push(i);
+                st1.push(i);
             }
             else{
-                if(!st2.empty()) ans[i]=st2.top()-i;
-                st1.push(temperatures[i]);
-                st2.push(i);
+                if(!st1.empty()) ans[i]=st1.top()-i;
+                st1.push(i);
             }
             i--;
         }
