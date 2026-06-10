8fab55c124de20a399df8ba6eeb9c00be70fb5a7
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-10 08:00:35 +0530

    Time: 7 ms (63.23%), Space: 13.4 MB (28.58%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -5,27 +5,28 @@ public:
         int n=word2.size();
         vector<vector<int>>dp(m+1,vector<int>(n+1,0));
         // j==n s2 is exhausted insert rem s1 char
+        //if j==0 then rem char = i because i dosenot denote index it denotes length of rem char
         for(int i=0;i<=m;i++){
-            dp[i][n]=m-i;
+            dp[i][0]=i;
         }
         //these formulas came from the recursive code
         for(int j=0;j<=n;j++){
-            dp[m][j]=n-j;
+            dp[0][j]=j;
         }
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                if(word1[i]==word2[j]){
-                    dp[i][j]=dp[i+1][j+1];
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
                 }
                 else{
-                    int rep = dp[i+1][j+1];
-                    int ins = dp[i][j+1];
-                    int del = dp[i+1][j];
+                    int rep = dp[i-1][j-1];
+                    int ins = dp[i][j-1];
+                    int del = dp[i-1][j];
                     dp[i][j]=1+min({rep,ins,del});
                 }
             }
         }
-        return dp[0][0];
+        return dp[m][n];
         // return (m-dp[m][n]);
     }
 };
