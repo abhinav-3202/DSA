e388e19b9ab73f14b6e7ed793e83eb2532ee0500
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-11 20:02:37 +0530

    Time: 57 ms (41.88%), Space: 15.8 MB (51.95%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -21,7 +21,32 @@ public:
     bool isMatch(string s, string p) {
         int m=s.size();
         int n=p.size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return check(s,p,m-1,n-1,dp);
+        // how base cases were thought , firstly convert <0 to ==0
+        // for 1 based indexing and >= to >0 only and write base case
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        vector<vector<bool>>dp(m+1,vector<bool>(n+1,false));
+        dp[0][0]=true;
+        // for(int i=1;i<=m;i++){
+        //     dp[i][0]=false;
+        // }  //no need for this initailization because already declared as false;
+        for(int j=1;j<=n;j++){
+            if(p[j-1]=='*'){
+                dp[0][j]=dp[0][j-1]; // we are taking true only if all previous were true or all previous were *
+                // else it is false only  
+            }
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                    dp[i][j]= dp[i][j-1] || dp[i-1][j];
+                }
+                else dp[i][j]=false;
+            }
+        }
+        return dp[m][n];
+        // return check(s,p,m-1,n-1,dp);
     }
 };
