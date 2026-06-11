7b36e119137f73d7304e3e9e5e345bbf398c54cc
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-11 12:54:29 +0530

    Time: 542 ms (24.24%), Space: 0B (100%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+ COUNT(DISTINCT user_id) AS active_users,
+ activity_date AS day
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY 
+activity_date;
+
+
+-- WE ARE NOT USING HAVING BECAUSE HAVIMG FILTERS AFTER GROUPING AND WE WANT TO THROW EVERYTHING THAT IS OUTSIDE THIS WINDOW 
