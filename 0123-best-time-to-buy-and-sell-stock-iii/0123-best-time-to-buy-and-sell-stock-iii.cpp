f6aadd86aa148437cb616aafcafe845706a6dced
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-13 07:44:14 +0530

    Time: 180 ms (50.76%), Space: 138.8 MB (52.46%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int check(vector<int>&prices,int i,int n,int flag,int count,vector<vector<vector<int>>>&dp){
+    int check(vector<int>&prices,int i,int n,int count,vector<vector<int>>&dp){
         if(i==n) return 0;
-        if(count==2) return 0;
-        if(dp[i][flag][count]!=-1) return dp[i][flag][count];
+        if(count==4) return 0;
+        if(dp[i][count]!=-1) return dp[i][count];
         int profit;
-        if(flag==1){
-            int buy=-prices[i]+check(prices,i+1,n,0,count,dp);
-            int notBuy = check(prices,i+1,n,1,count,dp);
+        if(count%2==0){
+            int buy=-prices[i]+check(prices,i+1,n,count+1,dp);
+            int notBuy = check(prices,i+1,n,count,dp);
             profit=max(buy,notBuy);
         }
         else{
-            int sell = prices[i]+check(prices,i+1,n,1,count+1,dp);
-            int notSell = check(prices,i+1,n,0,count,dp);
+            int sell = prices[i]+check(prices,i+1,n,count+1,dp);
+            int notSell = check(prices,i+1,n,count,dp);
             profit=max(sell,notSell);
         }
-        return dp[i][flag][count] = profit;
+        return dp[i][count] = profit;
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         int count=0;
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,
-        vector<int>(3,-1)));
-        return check(prices,0,n,1,count,dp);
+        vector<vector<int>>dp(n,vector<int>(4,-1));
+        return check(prices,0,n,count,dp);
     }
 };
