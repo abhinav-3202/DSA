041d44e8c0bd34f2c58384844d4a5c50cba60dcc
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-24 20:31:53 +0530

    Time: 13 ms (18.45%), Space: 27.6 MB (8.73%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,34 +2,32 @@ class Solution {
 public:
     int check(vector<int>&prices,int i ,int n,int flag,vector<vector<int>>&dp){
         if(i==n) return 0;
-        int profit = 0;
         if(dp[i][flag]!=-1) return dp[i][flag];
-        if(flag == 1){
-            int buy = -prices[i] + check(prices,i+1,n,0,dp);
-            int notBuy = check(prices,i+1,n,1,dp);
-            profit = max(buy,notBuy);
+        int profit;
+        if(flag==0){ // buy
+            int buy = -prices[i]+check(prices,i+1,n,1,dp);
+            int notBuy=check(prices,i+1,n,0,dp);
+            profit=max(buy,notBuy);
         }
         else{
-            int sell=0;
-            int notSell = 0;
-                // int profitt = prices[i]-prices[j];
-            sell = prices[i] + check(prices,i+1,n,1,dp);
-            notSell = check(prices,i+1,n,0,dp);
-            profit = max(sell,notSell);
+            int sell=prices[i]+check(prices,i+1,n,0,dp);
+            int notSell=check(prices,i+1,n,1,dp);
+            profit=max(sell,notSell);
         }
-        return dp[i][flag] = profit;
+        return dp[i][flag]=profit;
     }
     int maxProfit(vector<int>& prices) {
         // int maxi = 0;
-        int  flag = 1;
+        int  flag = 0;
         int n = prices.size();
-        // vector<vector<int>>dp(n,vector<int>(2,-1));
-        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return check(prices,0,n,flag,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(2,0));
         dp[n][0]=0;
         dp[n][1]=0;
         for(int i=n-1;i>=0;i--){
-            for(int j=0;j<=1;j++){
                 int profit;
+            for(int j=0;j<=1;j++){
                 if(j == 1){
                     int buy = -prices[i] + dp[i+1][0];
                     int notBuy = dp[i+1][1];
