66e63de81f5207cb88819feaf02cf7f7d5049bd2
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-25 07:41:21 +0530

    Time: 0 ms (100%), Space: 16.2 MB (51.11%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -17,9 +17,33 @@ public:
         return dp[i][flag]=profit;
     }
     int maxProfit(vector<int>& prices) {
-        int flag=1;
+        // int flag=1;
         int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return check(prices,0,n,flag,dp);
+        // vector<vector<int>>dp(n,vector<int>(2,-1));
+        vector<vector<int>>dp(n+2,vector<int>(2,0));
+        for(int i=n-1;i>=0;i--){
+            for(int flag=1;flag>=0;flag--){
+                int profit;
+                if(flag==1){
+                    // int buy=0;/
+                    // i
+                    int buy=-prices[i]+dp[i+1][0];
+                    int notBuy=dp[i+1][1];
+                    profit=max(buy,notBuy);
+                }
+                else{
+                    int sell=0;
+                    // if(i<=n-2){
+                    //     sell=prices[i]+dp[i+2][1];
+                    // } this is wrong because u should be able to sell on last day
+                    sell = prices[i]+dp[i+2][1];
+                    int notSell= dp[i+1][0];
+                    profit=max(sell,notSell);
+                }
+                dp[i][flag]=profit;
+            }
+        }
+        return dp[0][1];  // dp[0][1] because 1 is the state of buying
+        // return check(prices,0,n,flag,dp);
     }
 };
