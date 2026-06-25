702134a3536f00041c04807c98b2b60c0d8880e0
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-25 11:57:22 +0530

    Time: 3079 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-select emp.name as Employee
-from employee emp
-join employee mngr
-on emp.managerId=mngr.id  -- jo table join hua usme right table ki jo id hogi usko left table ke managerId ke equal kar do ...
-where emp.salary>mngr.salary;
+select e.name as Employee from Employee as e 
+join employee as e1 
+on e.managerId=e1.id 
+where e.salary>e1.salary;
