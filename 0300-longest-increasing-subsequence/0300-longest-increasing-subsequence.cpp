44ff293f98e80a978224dba9b1e2a13938369c2d
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-26 08:00:16 +0530

    Time: 615 ms (5.92%), Space: 296.6 MB (6.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,38 +1,20 @@
 class Solution {
-public: 
-    int check(vector<int>&nums,int i,int n,int prev_idx,vector<vector<int>>&dp){
+public:
+    int check(vector<int>&nums,int i,int n,int prev,vector<vector<int>>&dp){
         if(i==n) return 0;
-        // we haven't taken nums[prev_idx+1] +1 here because we have taken that value just for the dp matrix to satisfy the prev_idx+1 is only used in the dp only , other than that it is the original index of the array only 
-        if(dp[i][prev_idx+1]!=-1) return dp[i][prev_idx+1];
-        if(prev_idx+1==0 || nums[i]>nums[prev_idx]){
-            int pick= 1+check(nums,i+1,n,i,dp);
-            int notPick=check(nums,i+1,n,prev_idx,dp);
-            return dp[i][prev_idx+1]=max(pick,notPick);
+        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+        if(prev+1==0 || nums[i]>nums[prev]){
+            int pick = 1+check(nums,i+1,n,i,dp);
+            int notPick=check(nums,i+1,n,prev,dp);
+            return dp[i][prev+1]=max(pick,notPick);
         }
         else{
-            return dp[i][prev_idx+1]=check(nums,i+1,n,prev_idx,dp);
+            return dp[i][prev+1]=check(nums,i+1,n,prev,dp);
         }
-
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        // int prev_idx=-1;
-        // vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        for(int i=n-1;i>=0;i--){
-            //the prev_idx cannot be beyond or == idx so it will start from one step less
-            for(int prev_idx=i-1;prev_idx>=-1;prev_idx--){
-                if(prev_idx+1==0 || nums[i]>nums[prev_idx]){
-                    int pick= 1+dp[i+1][i+1];
-                    int notPick=dp[i+1][prev_idx+1];
-                    dp[i][prev_idx+1]=max(pick,notPick);
-                }
-                else{
-                    dp[i][prev_idx+1]=dp[i+1][prev_idx+1];
-                }
-            }
-        }
-        return dp[0][0];
-        // return check(nums,0,n,prev_idx,dp);
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return check(nums,0,n,-1,dp);
     }
 };
