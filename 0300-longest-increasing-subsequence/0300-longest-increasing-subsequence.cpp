78d62ae97c828c72ce5cfc31d51a7170c151cedc
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-27 07:53:43 +0530

    Time: 107 ms (39.43%), Space: 14.2 MB (69.69%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
-    int check(vector<int>&nums,int i,int n,int prev,vector<vector<int>>&dp){
-        if(i==n) return 0;
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        if(prev+1==0 || nums[i]>nums[prev]){
-            int pick = 1+check(nums,i+1,n,i,dp);
-            int notPick=check(nums,i+1,n,prev,dp);
-            return dp[i][prev+1]=max(pick,notPick);
-        }
-        else{
-            return dp[i][prev+1]=check(nums,i+1,n,prev,dp);
-        }
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        // vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        for(int i=n-1;i>=0;i--){
-            for(int prev=i-1;prev>=-1;prev--){
-                if(prev+1==0 || nums[i]>nums[prev]){
-                    int pick=1+dp[i+1][i+1];
-                    //i+1 is the prev because , prev is shifted by 1 index , ex -->>0 col prev is -1(-1+1=0) 
-                    // 1 col prev is 0 as (0+1=1) 
-                    int notPick=dp[i+1][prev+1];
-                    dp[i][prev+1]=max(pick,notPick);
+        vector<int>dp(n,1);
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(nums[prev]<nums[i]){
+                    dp[i]=max(1+dp[prev],dp[i]);
                 }
-                else{
-                    dp[i][prev+1]=dp[i+1][prev+1];
-                }
+                maxi=max(maxi,dp[i]);
             }
         }
-        return dp[0][0];
-        // return check(nums,0,n,-1,dp);
+        return maxi;
     }
 };
