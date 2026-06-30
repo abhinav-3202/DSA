04659cb1c7c7661b0a1fdf8497965a28b8b9c63a
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-06-30 07:39:17 +0530

    Time: 143 ms (34.68%), Space: 105.9 MB (28.76%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool compare(string& a , string& b){
+        int m=a.size();
+        int n=b.size();
+        if(m+1!=n) return false;
+        int i=0,j=0,count=0;
+        while(i<m){
+            if(count>1) return false;
+            if(a[i]!=b[j]){
+                count++;
+                j++;
+            }
+            else {
+                i++;
+                j++;
+            }
+        }
+        return true;
+    }
+    int check(int i,int prev,int n,vector<string>&words,vector<vector<int>>&dp){
+        if(i==n) return 0;
+        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+        if(prev+1==0 || compare(words[prev],words[i])){
+            int pick=1+check(i+1,i,n,words,dp);
+            int notPick=check(i+1,prev,n,words,dp);
+            return dp[i][prev+1]=max(pick,notPick);
+        }
+        else{
+            return dp[i][prev+1]=check(i+1,prev,n,words,dp);
+        }
+    }
+    int longestStrChain(vector<string>& words) {
+        int n=words.size();
+        sort(words.begin(),words.end(),[]( string& a , string& b){
+            return a.length()<b.length();
+        });
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return check(0,-1,n,words,dp);
+    }
+};
