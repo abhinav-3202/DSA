14170e541f698118c991a14e6e57af7f25cbaede
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-07-06 07:12:47 +0530

    Time: 139 ms (9.89%), Space: 91.2 MB (13.26%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        // map was giving TLE because iterating over duplicate elements also 
-        // so better use set data structure for unique elements
-        unordered_map<int,int>mpp;
-        unordered_set<int>st;
-        if(nums.size()==0) return 0;
-        if(nums.size()==1) return 1;
+        map<int,int>mpp;
         int n=nums.size();
-        for(int i =0;i<n;i++){
+        // if(n==1) return 1;
+        if(n==0) return 0;
+        for(int i=0;i<n;i++){
             mpp[nums[i]]++;
-            // st.insert(nums[i]);
         }
+        int maxi=1;
+        int prev=INT_MIN;
         int count=0;
-        int maxCount=0;
         for(auto it:mpp){
-            int y = it.first;
-            if(mpp.find(y-1)==mpp.end()){
-                int count=1;
-                int x=it.first+1;
-                while(mpp.find(x)!=mpp.end()){
-                    count++;
-                    x++;
-                }
-                maxCount=max(count,maxCount);
+            if(prev+1==it.first){
+                count++;
+                maxi=max(maxi,count);
+                prev=it.first;
+            }
+            else{
+                prev=it.first;
+                count=1;
             }
         }
-        // for(auto it:st){
-        //     if(st.find(it-1)==st.end()){
-        //         int count=1;
-        //         int x=it+1;
-        //         while(st.find(x)!=st.end()){
-        //             count++;
-        //             x++;
-        //         }
-        //         maxCount=max(count,maxCount);
-        //     }
-        // }
-        return maxCount;
+        return maxi;
     }
 };
