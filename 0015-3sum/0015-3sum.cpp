115a5787c44d990dfd3579b7534fa25498f7beb7
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-07-08 07:16:32 +0530

    Time: 47 ms (65.29%), Space: 29 MB (73.51%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
         int n=nums.size();
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            // vector<int>anss;
-            int j=i+1;
-            int k=n-1;
+        vector<vector<int>>ans;
+        for(int i =0;i<n-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;//for duplicates in starting 
             int val = -nums[i];
+            int j=i+1,k=n-1;
             while(j<k){
                 if(nums[j]+nums[k]==val){
                     ans.push_back({nums[i],nums[j],nums[k]});
-                    while(j<k && nums[j]==nums[j+1]) j++;
-                    while(j<k && nums[k]==nums[k-1]) k--;
-                    j++;
+                    while(j<k && nums[j]==nums[j+1]) j++;//duplicates
+                    while(j<k && nums[k]==nums[k-1]) k--;//duplicates
+                    j++; // jhere at 2,2 after while j will be at next so both have been used so j++ again
                     k--;
                 }
                 else if(nums[j]+nums[k]<val){
                     j++;
                 }
                 else k--;
-            }   
+            }
         }
         return ans;
     }
