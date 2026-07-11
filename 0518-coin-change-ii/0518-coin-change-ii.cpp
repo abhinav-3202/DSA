d46365028f7a82cc7fb0e8dc08f9f9a366f70a27
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-07-11 12:30:50 +0530

    Time: 52 ms (12.99%), Space: 36.9 MB (28.07%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    int check(vector<int>&coins,vector<vector<int>>&dp,int amount,int i){
+    int check(vector<int>&coins,int i,int n,int amount,vector<vector<int>>&dp){
         if(amount==0) return 1;
-        if(i==0){
-            if(amount%coins[0]==0) return 1;
-            else return 0;
-        }
+        if(i>=n || amount<0) return 0;
         if(dp[i][amount]!=-1) return dp[i][amount];
-        int notTake = check(coins,dp,amount,i-1);
-        int take = 0;
-        if(coins[i]<=amount) {
-            take=check(coins,dp,amount-coins[i],i);
-        }
-        return dp[i][amount] = take+notTake;
+        int pick = check(coins,i,n,amount-coins[i],dp);
+        int notPick = check(coins,i+1,n,amount,dp);
+        return dp[i][amount]=pick+notPick;
     }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int res= check(coins,dp,amount,n-1);
-        return res;
+        return check(coins,0,n,amount,dp);
     }
 };
