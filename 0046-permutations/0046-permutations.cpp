b898e4195d68a325fd4418652dfb92a3f387b9b5
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2026-07-28 15:16:53 +0530

    Time: 4 ms (10.27%), Space: 11.1 MB (18.35%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void check(vector<int>&temp,vector<vector<int>>&ans,unordered_map<int,int>&mpp,vector<int>& nums,int n){
+    void check(vector<int>&nums,int prev,int n,vector<int>&temp,vector<vector<int>>&ans,unordered_map<int,int>&mpp){ 
         if(temp.size()==n){
             ans.push_back(temp);
             return;
         }
         for(int i=0;i<n;i++){
-            if(mpp[i]==0){
+            if(mpp.find(nums[i])==mpp.end()){
                 temp.push_back(nums[i]);
-                mpp[i]=1;
-                check(temp,ans,mpp,nums,n);
+                mpp[nums[i]]++;
+                check(nums,i+1,n,temp,ans,mpp);
                 temp.pop_back();
-                mpp[i]=0;
+                mpp.erase(nums[i]);
             }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mpp;
         vector<int>temp;
         vector<vector<int>>ans;
-        unordered_map<int,int>mpp;
-        int n=nums.size();
-        check(temp,ans,mpp,nums,n);
+        check(nums,0,n,temp,ans,mpp);
         return ans;
     }
 };
